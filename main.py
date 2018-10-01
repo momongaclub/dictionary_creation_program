b3b03de64de25d689e3cc367c40456b4497484b2
Ryota_Mibayashi <threeforest8@gmail.com>
2018-10-02 01:55:59 +0900

    fixed some functions

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import argparse
 import re
 
 FILE_SEPARATER = '[\s/]'
+MIN_NUMBER = -1
+BRANK = ''
 
 
 def parse():
@@ -30,50 +32,23 @@ def load_input_file(fname):
     return word_dict
 
 
-def max_element(word_list):
+def max_element(pos_list):
     u"""
     word_listは要素が全て文字列のlistを仮定
     word_list内の最も多い要素を返す
     """
-    word_set_list = set(word_list)
-    for element in word_list:
-        cnt = 0
-        maxcnt = -1
-        for element in word_set_list:
-            cnt += 1 
-        if cnt > maxcnt:
-            maxcnt = cnt
-            maxelement = element
-    return maxelement
+    pos_set = set(pos_list)
+    max_NOO = MIN_NUMBER
+    max_pos = BRANK
 
+    for pos in pos_set:
+        NOO = pos_list.index(pos)# number of occurrences
+        if NOO > max_NOO:
+            max_NOO = NOO
+            max_pos = pos
 
-def equal_word_list(train_word_class, test_word_class):
-    u"""
-    train_word_classはstring,test_word_classはstringを仮定
-    train_word_classがtest_word_class同じであるかTrue,Falseを返す
-    """
-    if train_word_class == test_word_class:
-        return True
-    else:
-        return False
+    return max_pos
 
-
-def guess_word(word_dict, test_dict, word_list):
-    u"""
-    word_dictはkeyが文字列,要素が文字列のlist,word_listは要素が全て文字列のlistを仮定
-    word_list内全ての単語とその品詞を'単語/品詞'の形で出力する
-    """
-    cnt = 0
-    for word in word_list:
-        if word in word_dict:
-            print(word + '/' + max_element(word_dict[word]), end = " ")
-            if equal_word_list(max_element(word_dict[word]), max_element(test_dict[word])) == True:
-                cnt += 1
-            #print(max_element(word_dict[word]), max_element(test_dict[word]))
-        else:
-            cnt += 1
-            print(word + '/' + 'N', end=" ")
-    return len(word_list)-cnt
 
 def define_pos(word_dict, test_dict, word_list):
 
@@ -88,7 +63,6 @@ def main():
     test_dict = load_input_file(args.sample_test)
     dict_words_set = test_dict.keys()
     define_pos(train_dict, test_dict, dict_words_set)
-    #print(guess_word(train_dict, test_dict, dict_words_set))
 
 main()
 
